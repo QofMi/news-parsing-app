c4bd1abd971c8314ce575f0466778b579581d2d9
QofMi <avatar11132@gmail.com>
2021-02-11 11:03:53 +0700

    added scheduler

--- api/news_parsing/apps.py
+++ api/news_parsing/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class NewsParsingConfig(AppConfig):
     name = 'news_parsing'
     verbose_name = 'Новости'
+
+    def ready(self):
+        from .news_scheduler import news_updater
+        news_updater.start()
